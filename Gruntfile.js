f936864e5315ffc6a05b11d4f6198c77cbc6b21c
xuruiting1 <1263183452@qq.com>
2019-03-05 15:58:54 +0800

    add htmlhint

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    htmlhint{
+      html1: {
+        options: {
+                htmlhintrc: '.htmlhintrc'
+        
+        },
+      src: ['*.html']
+                           
+      }
+    }         
+  });
+
+    grunt.loadNpmTasks('grunt-htmlhint');
+
+      grunt.registerTask('default', ['htmlhint']);
+
+};
+
