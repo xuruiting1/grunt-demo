f2350891a2696622851c6356920a9da31a9c8df0
xuruiting1 <1263183452@qq.com>
2019-03-12 14:41:14 +0800

    add cssmin

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+        cssmin: {  
+              'rectangle.min.css': 'rectangle.css'
+            }
+                  
+  });
+
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', ['cssmin']); 
+
+};
